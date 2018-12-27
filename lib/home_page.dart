eed7b58c5150dbba35af6940275ce824beef2522
Harsh <Harsh Gupta>
2018-12-27 19:31:46 +0530

    home page progress

---
+++ lib/home_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> avatarCharacteristics = [
+    "Avatar Style",
+    "Top",
+    "Accessories",
+    "Facial Hair",
+    "Facial Hair Color",
+    "Clothes",
+    "Color Fabric",
+    "Eyes",
+    "Eyebrow",
+    "Mouth",
+    "Skin"
+  ];
+
+  Map<dynamic, String> currentSelected = {
+    "avatar_style":"Circle",
+    "top": "ShortHairShaggyMullet",
+    "accessories": "Kurt",
+    "hair_color": "BrownDark",
+    "facial_hair": "MoustacheMagnum",
+    "facial_hair_color": "Red",
+    "clothes": "GraphicShirt",
+    "color_fabric": "PastelRed",
+    "graphic": "Skull",
+    "eyes": "Close",
+    "eyebrow": "Angry",
+    "mouth": "Concerned",
+    "skin": "Tanned",
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text('kinzy'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          new Container(
+            color: Colors.blue,
+            height: 200.0,
+          ),
+          new Container(
+            height: 350.0,
+            child: new GridView.count(
+                crossAxisCount: 3,
+                childAspectRatio: 1.0,
+                padding: const EdgeInsets.all(4.0),
+                mainAxisSpacing: 4.0,
+                crossAxisSpacing: 4.0,
+                children: avatarCharacteristics.map((String characteristic) {
+                  return new Container(
+                    child: new Card(
+                      color: Colors.white,
+                      child: new FlatButton(
+                        onPressed: () {
+                          _showModalBottomSheet(context, characteristic);
+                        },
+                        child: new Text(
+                          characteristic,
+                          style: TextStyle(fontSize: 18.0, color: Colors.black),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  );
+                }).toList()),
+          )
+        ],
+      ),
+    );
+  }
+
+  _showModalBottomSheet(BuildContext context, String tabSelected) {
+
+    List<String> gridData = ["assets/coffee.jpeg","assets/girl.jpg","assets/coffee.jpeg","assets/girl.jpg",
+    "assets/coffee.jpeg","assets/girl.jpg","assets/coffee.jpeg","assets/girl.jpg",
+    "assets/coffee.jpeg","assets/girl.jpg","assets/coffee.jpeg","assets/girl.jpg"];
+
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 300.0,
+            child: new GridView.count(
+              crossAxisCount: 3,
+                children: gridData.map((String image) {
+                  return new FlatButton(
+                      onPressed: (){
+
+                      },
+                      child: new GridTile(
+                        child: new Image.asset(image,fit: BoxFit.fill,),
+                      )
+                  );
+                }).toList()),
+          );
+        });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_avataar/home_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: new Scaffold(
+        body: HomePage(),
+      ),
+    );
   }
 }
 
